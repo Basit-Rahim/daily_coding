16669899545e94d2569859f9f56308e700b8e762
basitr@shipin.ai <basitrahim66@gmail.com>
2025-11-25 20:24:59 +0500

    day 6

--- day6.cpp
+++ day6.cpp
@@ -43,15 +43,37 @@ vector<int> solution1(vector<int> arr){
     return arr;
 }
 
+vector<int> solution2(vector<int> arr){
+    vector<int> arrl;
+    vector<int> arrr;
+    int prodl = 1;
+    int prodr = 1;
+    int size = arr.size();
+
+    for (int i=0;i<arr.size();i++) {
+        arrl.push_back(prodl);
+        arrr.push_back (prodr);
+        prodl *= arr[i];
+        prodr *= arr[size-1-i];
+    }
+
+    for (int i=0;i<arr.size();i++) {
+        arr[i] = arrl[i] * arrr [size-1-i];
+    }
+
+    return arr;
+
+}
+
 int main(){
 
 
-    Test case 1: [1, 2, 3, 4, 5] -> [120, 60, 40, 30, 24]
+    // Test case 1: [1, 2, 3, 4, 5] -> [120, 60, 40, 30, 24]
     vector<int> arr1 = {1, 2, 3, 4, 5,0};
-    vector<int> result1 = solution1(arr1);
+    vector<int> result1 = solution2(arr1);
     
     cout << "Test 1:" << endl;
-    cout << "Input: [1, 2, 3, 4, 5]" << endl;
+    cout << "Input: [1, 2, 3, 4, 5,0]" << endl;
     cout << "Output: [";
     for (int i = 0; i < result1.size(); i++) {
         cout << result1[i];
@@ -61,7 +83,7 @@ int main(){
     
     // Test case 2: [3, 2, 1] -> [2, 3, 6]
     vector<int> arr2 = {3, 2, 1};
-    vector<int> result2 = solution1(arr2);
+    vector<int> result2 = solution2(arr2);
     
     cout << "Test 2:" << endl;
     cout << "Input: [3, 2, 1]" << endl;
